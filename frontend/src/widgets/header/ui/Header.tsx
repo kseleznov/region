3aa3bd8c005fdf53c2382277f8413077bd26ff91
knnseleznov <knnseleznov@gmail.com>
2026-05-31 17:58:28 +0100

    refactor: create useHeader hook & update Header component

--- frontend/src/widgets/header/ui/Header.tsx
+++ frontend/src/widgets/header/ui/Header.tsx
@@ -1,32 +1,13 @@
 "use client";
 
 import Image from "next/image";
-import { useSelectCityStore } from "@/features/select-city";
-import { ROUTES } from "@/shared/config/routes";
+import { MapPin } from "lucide-react";
 import { Button } from "@/shared/ui";
 import { RankBadge } from "@/entities/rank";
-import { MapPin } from "lucide-react";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useHeader } from "../model/useHeader";
 
 export function Header() {
-  const { selectedCity } = useSelectCityStore();
-  const router = useRouter();
-
-  function redirect() {
-    router.push(ROUTES.region);
-  }
-
-  const [userProgress] = useState({
-    placesVisited: 0,
-    rank: "Insider",
-    rankProgress: 24,
-    rankMax: 40,
-    districts: 3,
-    hiddenSpots: 7,
-    isNightExplorer: true,
-    isFoodHunter: true,
-  });
+  const { selectedCity, userProgress, redirect } = useHeader();
 
   return (
     <header className="flex justify-between px-[16px] py-[24px]">
